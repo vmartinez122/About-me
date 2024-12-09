b4beaf73e467128e7db3f3e8c5774a49d8e51a70
vmartinez <victormartinez12321@gmail.com>
2024-12-09 20:55:30 +0100

    Initial commit

---
+++ src/MartinezVictorMain.java
@@ -0,0 +1,10 @@
+public class MartinezVictorMain {
+    public static void main(String[] args) {
+        MartinezVictorMain programa = new MartinezVictorMain();
+        programa.inicio();
+    }
+
+    private void inicio(){
+        MartinezVictorPerfil miPerfil = new MartinezVictorPerfil();
+    }
+}
---
+++ src/MartinezVictorPerfil.java
@@ -0,0 +1,12 @@
+public class MartinezVictorPerfil {
+    private String name; //El nombre del perfil
+    private String story; //La historia personal del perfil
+    private String[] hobbies; //Colección de hobbies del perfil
+    private String[] foods; //Colección de comidas favoritas del perfil
+    private String[] funFacts; //Colección de datos curiosos
+
+    /*
+    ** La clase deberá tener métodos para acceder a estos atributos y métodos que devuelvan la información
+    ** de manera formateada para que sea fácil de mostrar por consola la información.
+    */
+}
